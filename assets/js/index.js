3d51958ac440da4fe2ba73fa0206a0708f1306c2
MudassirDev <mughalmudassir966@gmail.com>
2024-10-28 21:01:39 +0500

    interval && window.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
 const app = document.getElementById('application');
 
-function render() {
-    if (userLoggedIn != undefined) {
-        if (userLoggedIn) {
+
+let intervalId = setInterval(()=>{
+    if (window.userLoggedIn != undefined) {
+        clearInterval(intervalId);
+        intervalId = null;
+        
+        if (window.userLoggedIn == true) {
             app.innerHTML = `<p>This is homepage</p>`;
         } else {
             app.innerHTML = `<a href="/chakki-learning/pages/login.html">Login</a>`;
         }
-    } else {
-        return render();
     }
-}
-
-render();
+}, 100);
