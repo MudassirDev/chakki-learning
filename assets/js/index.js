28f5574999d664a04b43846e14121c22a15d931a
MudassirDev <mughalmudassir966@gmail.com>
2024-10-28 20:55:12 +0500

    recursion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,15 @@
 const app = document.getElementById('application');
 
-if (userLoggedIn) {
-    app.innerHTML = `<p>This is homepage</p>`;
-} else {
-    app.innerHTML = `<a href="/chakki-learning/pages/login.html">Login</a>`;
+function render() {
+    if (userLoggedIn != undefined) {
+        if (userLoggedIn) {
+            app.innerHTML = `<p>This is homepage</p>`;
+        } else {
+            app.innerHTML = `<a href="/chakki-learning/pages/login.html">Login</a>`;
+        }
+    } else {
+        return render();
+    }
 }
+
+render();
