0d4737ff51b305b62554d44c6476db0f43d7ad24
MudassirDev <mughalmudassir966@gmail.com>
2024-10-28 23:18:34 +0500

    ok

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,9 +7,22 @@ let intervalId = setInterval(()=>{
         intervalId = null;
         
         if (window.userLoggedIn == true) {
-            app.innerHTML = `<p>This is homepage</p>`;
+            app.innerHTML = `
+            <ul>
+                <li><a class="active" href="/chakki-learning/">Home</a></li>
+                <li><a class="active" href="/chakki-learning/pages/createcustomer.html">Create Customer</a></li>
+                <li><a class="active" href="/chakki-learning/pages/createorderforcustomer.html">Create Order (For Customer)</a></li>
+                <li><a class="active" href="/chakki-learning/pages/createorder.html">Create Order</a></li>
+                <li><a id="logoutBtn" href="#">Logout</a></li>
+            </ul>
+            `;
         } else {
-            app.innerHTML = `<a href="/chakki-learning/pages/login.html">Login</a>`;
+            app.innerHTML = `
+            <ul>
+                <li><a class="active" href="/chakki-learning/">Home</a></li>
+                <li><a href="/chakki-learning/pages/login.html">Login</a></li>
+            </ul>
+            `;
         }
     }
 }, 100);
