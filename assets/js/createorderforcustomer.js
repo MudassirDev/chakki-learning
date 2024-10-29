5aafceb4b1173a69a440908728ccb574b812268b
MudassirDev <mughalmudassir966@gmail.com>
2024-10-30 04:43:36 +0500

    fixed one more bug

--- assets/js/createorderforcustomer.js
+++ assets/js/createorderforcustomer.js
@@ -117,19 +117,19 @@ function createOrder() {
         remainingAmount: 0,
     }
 
-    if (sadaAtaAmount.trim != "") {
+    if (sadaAtaAmount.trim() != "") {
         order.items.sadaAta = {};
         order.items.sadaAta.amount = sadaAtaAmount;
         order.items.sadaAta.price = Number(sadaAtaPrice);
     }
 
-    if (chokarAmount.trim != "") {
+    if (chokarAmount.trim() != "") {
         order.items.chokar = {};
         order.items.chokar.amount = chokarAmount;
         order.items.chokar.price = Number(chokarPrice);
     }
 
-    if (specialAtaAmount.trim != "") {
+    if (specialAtaAmount.trim() != "") {
         order.items.specialAta = {};
         order.items.specialAta.amount = specialAtaAmount;
         order.items.specialAta.price = Number(specialAtaPrice);
