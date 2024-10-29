98743a09f21ceef7a21ffa0b8da397e3b0fc825d
MudassirDev <mughalmudassir966@gmail.com>
2024-10-29 20:47:54 +0500

    create customer form (not working)

---
+++ assets/js/createcustomer.js
@@ -0,0 +1,8 @@
+const createCustomerForm = document.getElementById('create-customer');
+
+createCustomerForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const formData = new FormData(createCustomerForm);
+    const name = formData.get("customer-name");
+    console.log(name)
+})
