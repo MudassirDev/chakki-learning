5e807abbee9e25bab30a87e7bc5b183ccf0a3608
MudassirDev <mughalmudassir966@gmail.com>
2024-10-29 21:01:28 +0500

    add to database

--- assets/js/createcustomer.js
+++ assets/js/createcustomer.js
@@ -1,9 +1,16 @@
 const createCustomerForm = document.getElementById('create-customer');
 
-createCustomerForm.addEventListener('submit', function(e) {
+createCustomerForm.addEventListener('submit', async function(e) {
     e.preventDefault();
     const formData = new FormData(createCustomerForm);
     const name = formData.get("customer-name");
     console.log(name)
-    console.log(firebase.db)
+    try {
+        const docRef = await firebase.addDoc(firebase.collection(firebase.db, "users"), {
+          name: name,
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
 })
