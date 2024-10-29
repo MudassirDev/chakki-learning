8a3bc03ab29700885b8fdd770240df3c6b26379a
MudassirDev <mughalmudassir966@gmail.com>
2024-10-29 21:17:47 +0500

    getting all customers

--- assets/js/createcustomer.js
+++ assets/js/createcustomer.js
@@ -1,4 +1,5 @@
 const createCustomerForm = document.getElementById('create-customer');
+const getCustomers = document.getElementById('getCustomers');
 
 createCustomerForm.addEventListener('submit', async function(e) {
     e.preventDefault();
@@ -6,12 +7,19 @@ createCustomerForm.addEventListener('submit', async function(e) {
     const name = formData.get("customer-name");
     console.log(name)
     try {
-        const response = await firebase.setDoc(firebase.doc(firebase.db, "Customers", name), {
+        await firebase.setDoc(firebase.doc(firebase.db, "Customers", name), {
             orders: [{name: "something"}]
         });
-        console.log(response)
         console.log("Customer Added");
     } catch (e) {
         console.error("Error adding document: ", e);
     }
+})
+
+getCustomers.addEventListener('click', async function() {
+    const querySnapshot = await firebase.getDocs(firebase.collection(firebase.db, "Customers"));
+    console.log(querySnapshot)
+    querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+    });
 })
