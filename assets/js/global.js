b95ff95b7188f495d5db4c0b6b1f15a6a73652fa
MudassirDev <mughalmudassir966@gmail.com>
2024-11-04 13:13:16 +0500

    adding user to global object

--- assets/js/global.js
+++ assets/js/global.js
@@ -2,6 +2,7 @@ firebase.onAuthStateChanged(firebase.auth, (user) => {
     if (user) {
         console.log("User is signed in");
         window.userLoggedIn = true;
+        window.user = user;
     } else {
         console.log("No user is signed in.");
         window.userLoggedIn = false;
