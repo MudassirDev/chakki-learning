603028b0a9ba4a05bfe944f0f5436debea89f07f
MudassirDev <mughalmudassir966@gmail.com>
2024-12-16 20:46:43 +0500

    changed firebase

--- assets/js/modules/firebase.js
+++ assets/js/modules/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCAmDmDM_rYndxnkhIzHBrP6x0JPtDupzU",
-    authDomain: "chakki-1ad9d.firebaseapp.com",
-    projectId: "chakki-1ad9d",
-    storageBucket: "chakki-1ad9d.appspot.com",
-    messagingSenderId: "400902058518",
-    appId: "1:400902058518:web:ac8d50e36db35a3ed63b37",
-    measurementId: "G-D9548MQV0C"
-  };
+  apiKey: "AIzaSyD9FE-wZtrZt9XFfcP4Sdy7N1wU1Joyo6A",
+  authDomain: "chakki-ddfff.firebaseapp.com",
+  projectId: "chakki-ddfff",
+  storageBucket: "chakki-ddfff.firebasestorage.app",
+  messagingSenderId: "992359809479",
+  appId: "1:992359809479:web:58223a23188074de2b59e2",
+  measurementId: "G-ZGLD1S5SWG"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
